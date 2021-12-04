0d0ab1705a165da113559cb2b8ce16ce4dea6509
apaaris <aparis@ethz.ch>
2021-12-04 11:19:46 +0100

    day4.2

---
+++ day4/day4.py
@@ -0,0 +1,21 @@
+#Credits to u/4HbQ
+
+import numpy as np
+n, *b = open("in.txt")                  
+
+b = np.loadtxt(b, int).reshape(-1,5,5)
+m = np.zeros(b.shape, dtype=bool);
+
+tries = map(int, n.split(','))
+lst = []
+
+for n in tries:           
+    np.place(m,b == n, True)
+    win = (m.all(1) |  m.all(2)).any(1) 
+    if win.any():
+        lst.append((b * ~m)[win].sum() * n)
+        b = b[~win]
+        m = m[~win]
+
+print("Task 1: " + str(lst[0]))
+print("Task 2: " + str(lst[len(lst)-1]))
