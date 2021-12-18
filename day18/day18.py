d71590c7d205cbfff2b23f0641e3f9c3e121bb65
Andrea Paris <64897205+apaaris@users.noreply.github.com>
2021-12-18 11:44:55 +0100

    Update day18.py

--- day18/day18.py
+++ day18/day18.py
@@ -1,121 +1,179 @@
-import sys
+#!/usr/bin/env python3
 
-with open("in.txt") as f:
-    nums = f.read().splitlines()
+from utils.all import *
+
+advent.setup(2021, 18)
+DEBUG = 'debug' in map(str.lower, sys.argv)
+if not DEBUG:
+	fin = advent.get_input()
+else:
+	fin = io.StringIO('''\
+[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]
+[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]
+[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]
+[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]
+[7,[5,[[3,8],[1,4]]]]
+[[2,[2,2]],[8,[8,1]]]
+[2,9]
+[1,[[[9,3],9],[[9,0],[0,7]]]]
+[[[5,[7,4]],7],1]
+[[[[4,2],2],6],[8,7]]
+''')
+
+# eprint(*fin, sep='', end='----- end of input -----\n\n'); fin.seek(0, 0)
+timer_start()
+
+def replace_num(s, start, end, newnum):
+	return s[:start] + str(newnum) + s[end:]
+
+def add_num(s, start, end, toadd):
+	other = int(s[start:end])
+	return replace_num(s, start, end, other + toadd)
+
+def explode(s):
+	depth = 0
+
+	for i, c in enumerate(s):
+		if c == '[':
+			depth += 1
+
+			if depth == 5:
+				for j in range(i + 1, len(s)):
+					if not s[j].isdigit():
+						# print(i, j, s[j], s[i:])
+						assert s[j] == ','
+						assert s[j + 1].isdigit()
+						break
+
+				for k in range(j + 1, len(s)):
+					if not s[k].isdigit():
+						# print(i, j, s[j], s[i:])
+						assert s[k] == ']'
+						break
+
+				a = int(s[i + 1:j])
+				b = int(s[j + 1:k])
+				# print(a, b)
+
+				# replace pair with 0
+				# eprint('      ', s)
+				s = replace_num(s, i, k + 1, 0)
+				# eprint('ab->0 ', s)
+
+				# go left
+				for j in range(i - 1, -1, -1):
+					if s[j].isdigit():
+						break
+
+				if j != 0: # left number found
+					for k in range(j - 1, -1, -1):
+						if not s[k].isdigit():
+							break
+
+					# eprint(k, j, s[k:j])
+					s = add_num(s, k + 1, j + 1, a)
+					# eprint('left+ ', s)
+
+				# go right
+				for j in range(i + 2, len(s)):
+					if s[j].isdigit():
+						break
+
+				if j != len(s) - 1: # right number found
+					for k in range(j + 1, len(s)):
+						if not s[k].isdigit():
+							break
+
+					# eprint(j, k, s[j:k])
+					s = add_num(s, j, k, b)
+					# eprint('right+', s)
+
+				return s, True
+
+		elif c == ']':
+			depth -= 1
+
+	return s, False
+
+def split(s):
+	for i, c in enumerate(s):
+		if not c.isdigit():
+			continue
+
+		for j in range(i + 1, len(s)):
+			if not s[j].isdigit():
+				break
+
+		n = int(s[i:j])
+		if n >= 10:
+			a = n // 2
+			b = n - a
+
+			s = replace_num(s, i, j, f'[{a},{b}]')
+			return s, True
+
+	return s, False
+
+def redux(s):
+	ok = True
+	while ok:
+		s, ok = explode(s)
+		if ok:
+			# eprint('e', s)
+			continue
+
+		s, ok = split(s)
+		# if ok:
+			# eprint('s', s)
+
+	return s
+
+def magnitude(lst):
+	if type(lst) is int:
+		return lst
+
+	left, right = lst
+	return 3 * magnitude(left) + 2 * magnitude(right)
 
 
-def parse(s, left):
-    if s[left] >= '0' and s[left] <= '9':
-        return Node(None, None, ord(s[left]) - ord('0')), left + 1
-    if s[left] == '[':
-        leftNode, newIndex = parse(s, left + 1)
-        rightNode, newIndex = parse(s, newIndex + 1)
-        return Node(leftNode, rightNode, None), newIndex + 1
+res = next(fin).rstrip()
+
+for line in map(str.rstrip, fin):
+	# eprint(line)
+	res = f'[{res},{line}]'
+	# eprint(' ', res)
+
+	res = redux(res)
+
+	# break
+# explode('[[[[[9,8],1],2],3],4]')
+
+ans = magnitude(eval(res))
+
+advent.print_answer(1, ans)
+# wait('Submit? ')
+# advent.submit_answer(1, ans)
 
 
-class Node(object):
-    def __init__(self, left, right, value):
-        self.left = left
-        self.right = right
-        self.value = value
 
-    def print(self):
-        if self.value is not None:
-            return str(self.value)
-        else:
-            return "[" + self.left.print() + "," + self.right.print() + "]"
+fin.seek(0, 0)
+lsts = list(map(str.rstrip, fin))
 
-    def magnitude(self):
-        if self.value is not None:
-            return self.value
-        return self.left.magnitude() * 3 + self.right.magnitude() * 2
+best = 0
+for a, b in product(lsts, lsts):
+	if a is b:
+		continue
 
-    def reduce(self):
-        global lastValue
-        global toAdd
-        global exploded
-        # print("reducing", self.print())
-        while True:
-            exploded = False
-            lastValue = None
-            rec(self, 0)
-            if not exploded:
-                if not rec2(self):
-                    break
-                # print("split   ", self.print())
-            else:
-                # print("exploded", self.print())
-                pass
-        return self
+	m = magnitude(eval(redux(f'[{a},{b}]')))
+	if m > best:
+		best = m
 
-    def add(self, other):
-        res = Node(self, other, None)
-        res.reduce()
-        return res
+	m = magnitude(eval(redux(f'[{b},{a}]')))
+	if m > best:
+		best = m
 
+ans = best
 
-def rec(node, depth):
-    global lastValue
-    global toAdd
-    global exploded
-    if exploded:
-        if toAdd is None:
-            return
-        if node.value is None:
-            rec(node.left, depth + 1)
-            rec(node.right, depth + 1)
-        else:
-            node.value += toAdd
-            toAdd = None
-        return
-
-    if depth >= 4 and node.value is None:
-        if lastValue is not None:
-            lastValue.value += node.left.value
-        toAdd = node.right.value
-        node.left = node.right = None
-        node.value = 0
-        exploded = True
-        return
-    if node.value is None:
-        rec(node.left, depth + 1)
-        rec(node.right, depth + 1)
-    else:
-        lastValue = node
-
-
-def rec2(node):
-    if node.value is not None and node.value >= 10:
-        node.left = Node(None, None, node.value // 2)
-        node.right = Node(None, None, node.value - node.value // 2)
-        node.value = None
-        return True
-    if node.value is not None:
-        return False
-    if rec2(node.left):
-        return True
-    if rec2(node.right):
-        return True
-    return False
-
-
-cur, discard = parse(nums[0], 0)
-print(cur.print())
-cur.reduce()
-for i in range(1, len(nums)):
-    other, discard = parse(nums[i], 0)
-    print(other.print())
-    cur = cur.add(other)
-    print(cur.print())
-
-print(cur.print(), cur.magnitude())
-
-maxMagnitude = 0
-for num1 in nums:
-    for num2 in nums:
-        if num1 == num2:
-            continue
-        x1, _ = parse(num1, 0)
-        x2, _ = parse(num2, 0)
-        maxMagnitude = max(maxMagnitude, x1.add(x2).magnitude())
-print(maxMagnitude)
+advent.print_answer(2, ans)
+# wait('Submit? ')
+# advent.submit_answer(2, ans)
